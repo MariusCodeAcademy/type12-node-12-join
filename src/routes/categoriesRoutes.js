544f583ccff82da28116b44fcc9819595c79b891
Marius <marius@example.com>
2022-09-06 11:59:12 +0300

    categories

--- src/routes/categoriesRoutes.js
+++ src/routes/categoriesRoutes.js
@@ -1,3 +1,13 @@
 // sukurti routeri categories
 
 // GET /api/categories - parsiuncia visas kategorijas
+
+// GET /api/categories/count - parsiuncia kiek kurioje kategorijoje yra postu
+
+const sql = `
+  SELECT categories.name AS category, COUNT(posts.p_id)
+  FROM categories
+  LEFT JOIN posts
+  ON posts.category_id = categories.c_id
+  GROUP BY categories.name;
+`;
