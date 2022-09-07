9ab4f8d3f042f8e04614fa8f2905b3eddba9f4b4
Marius <marius@example.com>
2022-09-07 09:36:17 +0300

    deleteCategory

--- src/model/categoryModel.js
+++ src/model/categoryModel.js
@@ -4,18 +4,37 @@ const db = require('../utils/db');
 
 async function getAllCategries() {
   // Model
-  try {
-    const sql = 'SELECT * FROM categories';
-    const [rows] = await db.query(sql);
-    return rows;
-  } catch (error) {
-    console.log('error getAllCategries ===', error);
-    return false;
-  }
+  const sql = 'SELECT * FROM categories';
+  const [rows] = await db.query(sql);
+  return rows;
 }
 
-async function getAllCategoriesCounts() {}
+async function getAllCategoriesCounts() {
+  const sql = `
+    SELECT categories.name AS category, COUNT(posts.p_id) AS postsCount
+    FROM categories
+    LEFT JOIN posts
+    ON posts.category_id = categories.c_id
+    GROUP BY categories.name;
+  `;
+  const [rows] = await db.query(sql);
+  return rows;
+}
+
+// deleteCategory(id) - modelio funkcija prideti kategorijai
+async function deleteCategory(id) {
+  const sql = 'DELETE FROM categories WHERE c_id = ? LIMIT 1';
+  const [rows] = await db.execute(sql, [id]);
+  console.log('rows ===', rows);
+  if (rows.affectedRows === 1) {
+    return true;
+  }
+  return false;
+}
+// addNewCategory() - modelio funkcija prideti kategorijai
 
 module.exports = {
   getAllCategries,
+  getAllCategoriesCounts,
+  deleteCategory,
 };
--- src/routes/categoriesRoutes.js
+++ src/routes/categoriesRoutes.js
@@ -1,7 +1,10 @@
 // sukurti routeri categories
 const express = require('express');
-const { getAllCategries } = require('../model/categoryModel');
-const db = require('../utils/db');
+const {
+  getAllCategries,
+  getAllCategoriesCounts,
+  deleteCategory,
+} = require('../model/categoryModel');
 
 const categoriesRouter = express.Router();
 
@@ -11,7 +14,6 @@ categoriesRouter.get('/', async (req, res) => {
   try {
     // Model
     const categories = await getAllCategries();
-
     // controller
     res.status(200).json(categories);
   } catch (error) {
@@ -23,15 +25,27 @@ categoriesRouter.get('/', async (req, res) => {
 // GET /api/categories/count - parsiuncia kiek kurioje kategorijoje yra postu su pool
 categoriesRouter.get('/count', async (req, res) => {
   try {
-    const sql = `
-    SELECT categories.name AS category, COUNT(posts.p_id) AS postsCount
-    FROM categories
-    LEFT JOIN posts
-    ON posts.category_id = categories.c_id
-    GROUP BY categories.name;
-  `;
-    const [rows] = await db.query(sql);
-    res.status(200).json(rows);
+    const catsAndCounts = await getAllCategoriesCounts();
+    res.status(200).json(catsAndCounts);
+  } catch (error) {
+    console.log('error ===', error);
+    res.status(500).json({ msg: 'some stuff went sideways' });
+  }
+});
+
+// DELETE /api/categories/:id - istrinam irasa
+categoriesRouter.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    if (await deleteCategory(id)) {
+      res.status(200).json({
+        msg: 'category deleted',
+      });
+    } else {
+      res.status(400).json({
+        msg: 'nothing deleted',
+      });
+    }
   } catch (error) {
     console.log('error ===', error);
     res.status(500).json({ msg: 'some stuff went sideways' });
