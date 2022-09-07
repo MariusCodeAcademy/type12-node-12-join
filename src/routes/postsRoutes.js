516b5a7de52728b7a998f5ddb5ccdb46c878fe0e
Marius <marius@example.com>
2022-09-07 10:40:15 +0300

    /api/posts/all-info

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -26,4 +26,27 @@ postsRouter.get('/', async (req, res) => {
   }
 });
 
+// GET /api/posts/all-info
+postsRouter.get('/all-info', async (req, res) => {
+  try {
+    const conn = await mysql.createConnection(dbConfig);
+    const sql = `
+    SELECT posts.p_id, posts.title, posts.body, categories.name AS category, users.email, users.town
+    FROM posts
+    LEFT JOIN categories
+    ON posts.category_id = categories.c_id
+    LEFT JOIN users
+    ON posts.user_id = users.u_id
+    `;
+    const [rows] = await conn.query(sql);
+    res.status(200).json(rows);
+    await conn.end();
+  } catch (error) {
+    console.log('error ===', error);
+    res.status(500).json({
+      msg: 'Something went wrong',
+    });
+  }
+});
+
 module.exports = postsRouter;
