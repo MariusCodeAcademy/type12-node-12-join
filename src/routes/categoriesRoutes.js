88845a6faf754506a4f1969a80930f82aa630781
Marius <marius@example.com>
2022-09-07 08:50:49 +0300

    /api/categories/count

--- src/routes/categoriesRoutes.js
+++ src/routes/categoriesRoutes.js
@@ -5,6 +5,7 @@ const db = require('../utils/db');
 const categoriesRouter = express.Router();
 
 // GET /api/categories - parsiuncia visas kategorijas
+
 categoriesRouter.get('/', async (req, res) => {
   try {
     const sql = 'SELECT * FROM categories';
@@ -16,14 +17,22 @@ categoriesRouter.get('/', async (req, res) => {
   }
 });
 
-// GET /api/categories/count - parsiuncia kiek kurioje kategorijoje yra postu
-
-const sql = `
-  SELECT categories.name AS category, COUNT(posts.p_id)
-  FROM categories
-  LEFT JOIN posts
-  ON posts.category_id = categories.c_id
-  GROUP BY categories.name;
-`;
+// GET /api/categories/count - parsiuncia kiek kurioje kategorijoje yra postu su pool
+categoriesRouter.get('/count', async (req, res) => {
+  try {
+    const sql = `
+    SELECT categories.name AS category, COUNT(posts.p_id) AS postsCount
+    FROM categories
+    LEFT JOIN posts
+    ON posts.category_id = categories.c_id
+    GROUP BY categories.name;
+  `;
+    const [rows] = await db.query(sql);
+    res.status(200).json(rows);
+  } catch (error) {
+    console.log('error ===', error);
+    res.status(500).json({ msg: 'some stuff went sideways' });
+  }
+});
 
 module.exports = categoriesRouter;
